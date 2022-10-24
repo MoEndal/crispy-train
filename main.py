b2897bfb37b5437ecaf472d1c099f8a6ce5e7472
Joona <joona.rautiainen@brightstraining.com>
2022-10-24 16:52:19 +0300

    Joonan koodit last

---
+++ main.py
@@ -0,0 +1,11 @@
+correct_answers = 0
+question_number = 0
+
+#Kysymyksiä - lista?
+questions = [("Missä kaupungissa ystäväsi asuu?", "Helsinki", "Tampere", "AWS city", "Pori", "c", "Etkö edes tiedä missä ystäväsi asuu?"),("Kuinka monta olutta juot junamatkalla?", "0", "3", "6", "12", "d", "Etkö osaa juoda tarpeeksi? Häpeä.")]
+
+
+play_the_game = input("Lähdetkö ystävän luokse junan kyydillä? (n/y)")
+
+if play_the_game == "n":
+    print("Jää sitten kotiin! ")
